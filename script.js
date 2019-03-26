9e999258205ffdef9aec5902f5cb434584676799
Sebas443 <33268355+Sebas443@users.noreply.github.com>
2019-03-26 10:08:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+$(".Mini1").click(function(){
+var active=$(".Gallery-active");
+active.removeClass("Gallery-active");
+$(".Gallery1").addClass("Gallery-active");
+});
+
+
+$(".Mini2").click(function(){
+var active=$(".Gallery-active");
+active.removeClass("Gallery-active");
+$(".Gallery2").addClass("Gallery-active");
+});
+
+
+$(".Mini3").click(function(){
+var active=$(".Gallery-active");
+active.removeClass("Gallery-active");
+$(".Gallery3").addClass("Gallery-active");
+});
+
+
+$(".Mini4").click(function(){
+var active=$(".Gallery-active");
+active.removeClass("Gallery-active");
+$(".Gallery4").addClass("Gallery-active");
+});
+
+
+$(".Mini5").click(function(){
+var active=$(".Gallery-active");
+active.removeClass("Gallery-active");
+$(".Gallery5").addClass("Gallery-active");
+});
+
+});
